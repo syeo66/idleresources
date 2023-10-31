5c9aa7236daa8b3e1cc60d568fc8381db62be987
Red Ochsenbein <red@redochsenbein.ch>
2023-10-31 21:35:22 +0100

    Set new water value on click

--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/index.html"))
-    err := tmpl.Execute(w, nil)
+func water(w http.ResponseWriter, r *http.Request) {
+  tmpl := template.Must(template.ParseFiles("templates/water.html"))
+  err := tmpl.Execute(w, nil)
 
-    if err != nil {
-      fmt.Print(err)
-    }
+  if err != nil {
+    fmt.Print(err)
   }
+}
 
 func main() {
-  http.HandleFunc("/", index)
+  fileServer := http.FileServer(http.Dir("./static"))
+  http.Handle("/", fileServer)
+  http.HandleFunc("/water", water)
 
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
