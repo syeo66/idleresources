c155e79f4e9338390a192f5efd92d54d223f4fb1
Red Ochsenbein <red@redochsenbein.ch>
2023-10-31 21:55:35 +0100

    go fmt

--- main.go
+++ main.go
@@ -8,19 +8,18 @@ import (
 )
 
 func water(w http.ResponseWriter, r *http.Request) {
-  tmpl := template.Must(template.ParseFiles("templates/water.html"))
-  err := tmpl.Execute(w, nil)
+	tmpl := template.Must(template.ParseFiles("templates/water.html"))
+	err := tmpl.Execute(w, nil)
 
-  if err != nil {
-    fmt.Print(err)
-  }
+	if err != nil {
+		fmt.Print(err)
+	}
 }
 
 func main() {
-  fileServer := http.FileServer(http.Dir("./static"))
-  http.Handle("/", fileServer)
-  http.HandleFunc("/water", water)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/water", water)
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
