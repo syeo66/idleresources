98c3549ef67258472d073750079be0ad728c94b8
Red Ochsenbein <red@redochsenbein.ch>
2023-10-31 20:26:21 +0100

    Serve "Hello world"

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "html/template"
+  "net/http"
+)
+
+func main() {
+  tmpl := template.Must(template.ParseFiles("templates/index.html"))
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    tmpl.Execute(w, nil)
+  })
+  http.ListenAndServe(":8080", nil)
+}
+
